bbca303c39933fcc0324f1e12bf0a9d0f6c7b5a5
Lucasr <lucasrno15@gmail.com>
2025-07-07 12:56:03 -0400

    Esta implementacao defina a funcao responsavel por processar um arquivo texto de entrada, contendo um automato finito deterministico seguindo obrigatoriamente o formato proposto

--- src/afd.cpp
+++ src/afd.cpp
@@ -1,9 +1,13 @@
 #include <algorithm>
-#include <iostream>
-#include <string>
+#include <iostream>   
+#include <fstream>   
+#include <sstream>   
+#include <string>     
+#include <set>        
+#include <map>        
 #include <utility>
-
 #include "../include/afd.h"
+
 
 void Afd::processarPalavra(const std::string& palavra) const {
     std::string estadoAtual = this->estadoInicial;
@@ -32,4 +36,65 @@ void Afd::processarPalavra(const std::string& palavra) const {
         std::cout << "ACEITA\n";
     else
         std::cout << "REJEITA\n";
+}
+
+
+// metodo para ler um automato finito deterministico de um arquivo texto
+void Afd::lerDeArquivo(const std::string& nomeArquivo) {
+    std::ifstream arquivo(nomeArquivo);
+
+    std::string linha;
+    int linhaAtual = 0;
+
+    while (std::getline(arquivo, linha)) {
+        linhaAtual++;
+
+        if (linhaAtual == 1) {
+            auto inicio = linha.find('{') + 1;
+            auto fim = linha.find('}');
+            std::string conteudo = linha.substr(inicio, fim - inicio);
+
+            for (char c : conteudo) {
+                if (c != ',') {
+                    alfabeto += c;
+                }
+            }
+        }
+
+        else if (linhaAtual == 2) {
+            auto inicio = linha.find('{') + 1;
+            auto fim = linha.find('}');
+            std::string conteudo = linha.substr(inicio, fim - inicio);
+
+            std::stringstream ss(conteudo);
+            std::string estado;
+            while (std::getline(ss, estado, ',')) {
+                estados.insert(estado);
+            }
+        }
+
+        else if (linhaAtual == 3) {
+            auto inicio = linha.find('{') + 1;
+            auto fim = linha.find('}');
+            std::string conteudo = linha.substr(inicio, fim - inicio);
+            std::stringstream ss(conteudo);
+            std::string estado;
+            while (std::getline(ss, estado, ',')) {
+                estadosFinais.insert(estado);
+            }
+        }
+
+        else {
+            auto pos1 = linha.find('(');
+            auto pos2 = linha.find(',');
+            auto pos3 = linha.find(')');
+            auto pos4 = linha.find('=') + 1;
+            std::string origem = linha.substr(pos1 + 1, pos2 - pos1 - 1);
+            char simbolo = linha[pos2 + 1];
+            std::string destino = linha.substr(pos4 + 1);
+            while (destino[0] == ' ') destino.erase(0, 1); 
+            transicoes[{origem, simbolo}] = destino;
+        }
+    }
+    estadoInicial = "q0"; 
 }
